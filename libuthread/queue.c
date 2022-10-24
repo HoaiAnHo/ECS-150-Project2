9c050613215daffcb4497aeb0c70c45ff46d1ee2
HoaiAnHo <hoaianho3100@gmail.com>
2022-10-23 22:14:59 -0700

    queue
    none of this was tested and it was made assuming that there's a limit to the queue

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,41 +4,106 @@
 
 #include "queue.h"
 
+#define FULL 100
+
 struct queue {
-	/* TODO Phase 1 */
+    /* TODO Phase 1 */
+	int* arr;
+    int front;
+    int back;
+	int queue_len;
+	//we're queueing and dequeing addresses
 };
+
+// queue_t should point to an address
+// that address should contain the data?
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	queue_t init_queue;
+	int queue_arr[FULL];
+	init_queue->arr = queue_arr;
+	init_queue->queue_len = 0;
+	init_queue->front = -1;
+	init_queue->back = -1;
+	return init_queue;
 }
 
 int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
+	if (queue->queue_len != 0 || queue == NULL){
+		return -1;
+	}
+	// Deallocate the memory associated to the queue object pointed by @queue
+	// for every queue->arr[i], free(queue->arr[i])
+	return 0;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+	if (queue == NULL || data == NULL) {
+		return -1;
+	}
+	// if the queue can be added to
+	if (queue->back < FULL - 1){
+		if (queue->front == -1) queue->front = 0;
+		queue->back += 1;
+		queue->arr[queue->back] = &data;
+	}
+	// if queue is full
+	else{
+		return -1;
+	}
+	queue->queue_len += 1;
+	return 0;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
+	if (queue->queue_len == 0 || queue == NULL || data == NULL){
+		return -1;
+	}
+	else{
+		*data = queue->arr[queue->front];
+		queue->front += 1;
+	}
+	return 0;
 }
 
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+	if (queue->queue_len == 0 || queue == NULL || data == NULL){
+		return -1;
+	}
+	int i = queue->front;
+	while(1){
+		if (i > queue->back) return -1;
+		if (queue->arr[i] == data){
+			// delete item in queue->arr
+			return 0;
+		}
+	}
 }
 
 int queue_iterate(queue_t queue, queue_func_t func)
 {
-	/* TODO Phase 1 */
+	if (queue->queue_len == 0 || queue == NULL || func == NULL){
+		return -1;
+	}
+	int i = queue->front;
+	while(i <= queue->back){
+		if (queue->arr[i] == NULL)
+		{
+			i++;
+			continue;
+		}
+		func(queue, queue->arr[i]);
+		i++;
+	}
+	return 0;
 }
 
 int queue_length(queue_t queue)
 {
-	/* TODO Phase 1 */
+	return queue->queue_len;
 }
