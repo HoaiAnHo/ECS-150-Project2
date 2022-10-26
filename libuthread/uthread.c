3130f11622a5b65af91059055d9036780248f974
goldenfries100 <71404669+goldenfries100@users.noreply.github.com>
2022-10-26 15:44:04 -0700

    Add files via upload

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -15,7 +15,7 @@ struct uthread_tcb {
 
 struct uthread_tcb *uthread_current(void)
 {
-	/* TODO Phase 2/4 */
+	/* TODO Phase 2/3 */
 }
 
 void uthread_yield(void)
@@ -33,17 +33,18 @@ int uthread_create(uthread_func_t func, void *arg)
 	/* TODO Phase 2 */
 }
 
-int uthread_run(uthread_func_t func, void *arg)
+int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
 }
 
 void uthread_block(void)
 {
-	/* TODO Phase 4 */
+	/* TODO Phase 3 */
 }
 
 void uthread_unblock(struct uthread_tcb *uthread)
 {
-	/* TODO Phase 4 */
+	/* TODO Phase 3 */
 }
+
