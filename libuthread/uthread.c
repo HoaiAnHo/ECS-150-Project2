ad74d9015142dc90ddcd66bd9f0a5b62d41cbd51
HoaiAnHo <hoaianho3100@gmail.com>
2022-10-28 01:28:26 -0700

    Update uthread.c
    current work

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -9,8 +9,16 @@
 #include "private.h"
 #include "uthread.h"
 
+enum state {
+	running = 0,
+	ready = 1,
+	exited = 2
+}
+
 struct uthread_tcb {
-	/* TODO Phase 2 */
+	uthread_ctx_t context;
+	void stack;
+	int u_state;
 };
 
 struct uthread_tcb *uthread_current(void)
@@ -20,7 +28,7 @@ struct uthread_tcb *uthread_current(void)
 
 void uthread_yield(void)
 {
-	/* TODO Phase 2 */
+	// uthread_ctx_switch();
 }
 
 void uthread_exit(void)
@@ -30,12 +38,31 @@ void uthread_exit(void)
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+	uthread_ctx_t new_context;
+	void new_stack;
+	new_stack = uthread_ctx_alloc_stack();
+	uthread_ctx_init(*new_context, *new_stack, func, *arg);
 }
 
 int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+	if (preempt){
+		// preemptive scheduling is enabled
+	}
+	// register so-far single execution flow of app as the idle thread
+	// create initial thread
+	uthread_ctx_t init_context;
+	void *init_stack;
+	init_stack = uthread_ctx_alloc_stack();
+	struct uthread_tcb new_thread = {init_context, init_stack, ready};
+	uthread_ctx_init(*init_context, *init_stack, func, *arg);
+	// uthread_current should point to new_thread
+	while(1){
+		// run through availible threads
+		// if no more threads, return 0
+		// uthread_yield()
+		return 0;
+	}
 }
 
 void uthread_block(void)
