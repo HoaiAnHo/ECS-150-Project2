5279f638edddcfabf99edaff8232f762c875f1d9
goldenfries100 <71404669+goldenfries100@users.noreply.github.com>
2022-10-30 01:53:17 -0700

    added new unit test cases

--- apps/queue_tester.c
+++ apps/queue_tester.c
@@ -48,18 +48,47 @@ void test_simple_create_and_destroy()
 	TEST_ASSERT(!queue_destroy(q));
 }
 
-void test_queue_delete()
+void test_queue_delete_end()
 {
 	int data = 4, data_two = 5;
 	queue_t q;
-	fprintf(stderr, "*** TEST queue_delete ***\n");
+	fprintf(stderr, "*** TEST queue_delete end val***\n");
 
 	q = queue_create();
 	queue_enqueue(q, &data);
 	queue_enqueue(q, &data_two);
 	queue_delete(q, &data_two);
 
-	TEST_ASSERT(queue_length(q) == 1); //currently failing this test case
+	TEST_ASSERT(queue_length(q) == 1);
+}
+
+void test_queue_delete_middle()
+{
+	int data = 4, data_two = 5, data_three = 6;
+	queue_t q;
+	fprintf(stderr, "*** TEST queue_delete middle val***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data_two);
+	queue_enqueue(q, &data_three);
+	queue_delete(q, &data_two);
+
+	TEST_ASSERT(queue_length(q) == 2);
+}
+
+void test_queue_delete_front() //edge case of only 2 vals, need to reset q->back to NULL
+{
+	int data = 4, data_two = 5;
+	queue_t q;
+	fprintf(stderr, "*** TEST queue_delete front val***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data_two);
+	queue_delete(q, &data);
+
+	TEST_ASSERT(queue_length(q) == 1); //currently has a seg fault
 }
 
 int main(void)
@@ -67,7 +96,9 @@ int main(void)
 	test_create();
 	test_queue_simple();
 	test_simple_create_and_destroy();
-	test_queue_delete();
+	test_queue_delete_end();
+	test_queue_delete_middle();
+	test_queue_delete_front();
 
 	return 0;
 }
