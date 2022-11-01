88b60aa3a1ce0495e779a4491e0e912b1a179920
goldenfries100 <71404669+goldenfries100@users.noreply.github.com>
2022-10-31 23:53:26 -0700

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -10,13 +10,15 @@
 #include "uthread.h"
 #include "queue.h"
 
-queue_t ready_queue = queue_create();
-queue_t running_queue = queue_create();
+queue_t ready_queue; 
+queue_t blocked_queue; //push running thread to this queue
+//front of queue will always be running
 
 enum state {
 	running = 0,
 	ready = 1,
-	exited = 2
+	blocked = 2,
+	exited = 3
 };
 
 struct uthread_tcb {
@@ -28,73 +30,90 @@ struct uthread_tcb {
 struct uthread_tcb *uthread_current(void)
 {
 	/* TODO Phase 2/3 */
-	// return either front of ready or front of running
-	// return running_queue->front;
-	// why won't the queue point to any of its members???
+	return (struct uthread_tcb *) ready_queue->front->node_data;
 }
 
 void uthread_yield(void)
 {
-	// get running thread and front of ready queue
-	// uthread_ctx_switch(pointer of running context, pointer of front context);
-	// switch threads from running to ready and vice versa
+	struct uthread_tcb *yield_to;
+	queue_dequeue(ready_queue, yield_to);
+	queue_enqueue(blocked_queue, uthread_current());
+	uthread_ctx_switch(uthread_current(), &yield_to);
+	// don't forget state checking and changing before ctx switch
+	//update state
 }
 
 void uthread_exit(void)
 {
-	// first get thread
-	// remove thread from queue
 	// uthread_ctx_destroy_stack(void *top_of_stack)
-	// maybe destroy context?
-	// queue_delete(ready_queue, thread)
+	//update state
 }
 
-int uthread_create(uthread_func_t func, void *arg)
+int uthread_create(uthread_func_t func, void *arg) //done
 {
+	if (arg == NULL || func == NULL)
+		return -1;
+	
+	// create initial thread
 	uthread_ctx_t new_context;
 	void * new_stack;
 	new_stack = uthread_ctx_alloc_stack();
+	struct uthread_tcb * new_thread = (struct uthread_tcb *) malloc(sizeof (struct uthread_tcb));
+	new_thread->context = new_context;
+	new_thread->stack = new_stack;
+	new_thread->u_state = ready;
 	uthread_ctx_init(&new_context, &new_stack, func, &arg);
+
+	// add initial thread to queue
+	queue_enqueue(ready_queue, &new_thread);
+
+	// uthread_run(&new_thread, func(arg));
+
+
+	return 0;
 }
 
 int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
+	ready_queue = (struct queue *) malloc(sizeof (struct queue )); //fix this
+	ready_queue = queue_create();
+	blocked_queue = (struct queue *) malloc(sizeof (struct queue)); //fix this
+	blocked_queue = queue_create();
+	//phase 4 - preemption
 	if (preempt){
-		// preemptive scheduling is enabled (phase 4)
+		// preemptive scheduling is enabled
 		// preempt_enable();
 	}
 	// register so-far single execution flow of app as the idle thread
 
-	// create initial thread
+	// create idle thread
 	uthread_ctx_t init_context;
 	void * init_stack;
 	init_stack = uthread_ctx_alloc_stack();
 	struct uthread_tcb * idle_thread = (struct uthread_tcb *) malloc(sizeof (struct uthread_tcb));
 	idle_thread->context = init_context;
 	idle_thread->stack = init_stack;
-	idle_thread->u_state = ready;
-	uthread_ctx_init(idle_thread, idle_thread->stack, func, arg);
-
-	// add initial thread to queue
-	queue_enqueue(ready_queue, *idle_thread); // looks like syntax issue was because the queue name didn't exist anymore
-	// question, where do we get the other threads???
-	// create threads??? uthread_create(func, arg)
-	// is this file assuming we're only running one thread???
+	idle_thread->u_state = running;
+	//uthread_ctx_init(idle_thread, idle_thread->stack, func, arg);
+	
+	// add initial thread
+	uthread_create(func, &arg);
 
 	// uthread_current should point to new_thread
 	struct uthread_tcb *current;
 	current = uthread_current();
 
-	while(current){
-		// run through availible threads
-		if (current) {
-			// run thread
-		}
-		// uthread_yield()
-		// if no more threads, return 0
-			// destroy ready_queue and running_queue
-		return 0;
+	while(current != NULL){
+		// run through availible thread
+		uthread_yield();
+		// after function is done, remove from queue
+		queue_delete(ready_queue, current);
+		// get another thread from queue? current?
 	}
+	// if no more threads, return 0
+	queue_destroy(ready_queue);
+	queue_destroy(blocked_queue);
+	return 0;
 }
 
 void uthread_block(void)
