28f7d8bfbc74c6b8d8e349fb766ae0c538da7bd5
goldenfries100 <divyaraj940@gmail.com>
2022-11-03 01:01:12 -0700

    added sem skeleton

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -5,29 +5,65 @@
 #include "sem.h"
 #include "private.h"
 
+#include "uthread.h"
+
 struct semaphore {
 	/* TODO Phase 3 */
-	queue_t sem_queue;
-	int queue_count;
+	//queue_t sem_queue;
+	int lock;
+	size_t count;
 };
 
 sem_t sem_create(size_t count)
 {
 	/* TODO Phase 3 */
-	queue_t sem_queue = queue_create();
+	//queue_t sem_queue = queue_create();
+	sem_t sem = (struct semaphore *) malloc(sizeof(struct semaphore));
+	sem->lock = 0;
+	sem->count = count;
+	return sem;
 }
 
 int sem_destroy(sem_t sem)
 {
 	/* TODO Phase 3 */
+
+	if (sem == NULL || sem->count != 0 || sem->lock) //if locks are still in use, return -1
+		return -1;
+	
+	free(sem);
+	return 0;
 }
 
 int sem_down(sem_t sem)
 {
 	/* TODO Phase 3 */
+	if (sem == NULL)
+		return -1;
+
+	//spinlock_lock(sem->lock);
+	sem->lock = 1;
+	while (sem->count == 0) {
+		/* Block self */
+	}
+
+	sem->count -= 1;
+	//spinlock_unlock(sem->lock);
+	return 0;
 }
 
 int sem_up(sem_t sem)
 {
 	/* TODO Phase 3 */
+	if (sem == NULL)
+		return -1;
+
+	//spinlock_lock(sem->lock);
+	sem->count += 1;
+
+	/* wake up first thread in line, if there are any*/
+
+	//spinlock_unlock(sem->lock);
+	sem->lock = 0;
+	return 0;
 }
