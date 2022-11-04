cc14200e5b9f6252d29e8ffb56b407050a644925
HoaiAnHo <hoaianho3100@gmail.com>
2022-11-03 23:49:19 -0700

    Update queue.c
    cleaning up some queue code to make it neater

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -1,23 +1,18 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h> //for debugging, remove later
 #include <string.h>
 
 #include "queue.h"
 
 #define FULL 100
 
-int is_iterating = 0; //boolean to check if in queue iterate
+int is_iterating = 0; // boolean to check if in queue iterate
 
 struct node {
-	struct node* prev; //node infront of current
-	struct node* next; //node behind current
-	void * node_data; //changing void * int *
+	struct node* prev;
+	struct node* next;
+	void * node_data;
 };
-
-/*
- - context init (given for uthread create)
-*/
 
 struct queue {
     struct node* front;
@@ -27,6 +22,7 @@ struct queue {
 
 queue_t queue_create(void)
 {
+	// allocate space for queue, then assign values to struct members
 	queue_t init_queue = (struct queue *) malloc(sizeof (struct queue));
 	init_queue->back = NULL;
 	init_queue->front = NULL;
@@ -34,8 +30,9 @@ queue_t queue_create(void)
 	return init_queue;
 }
 
-int queue_destroy(queue_t queue) //just need to implement freeing parts
+int queue_destroy(queue_t queue) 
 {
+	// just need to implement freeing parts
 	if (queue->queue_len != 0 || queue == NULL){
 		return -1;
 	}
@@ -52,27 +49,22 @@ int queue_enqueue(queue_t queue, void *data)
 	}
 
 	if (queue->queue_len == 0) {
-		//printf("QUEUE IS EMPTY\n");
 		struct node* newNode = (struct node*) malloc(sizeof (struct node));
 		newNode->prev = NULL;
 		newNode->next = NULL;
 		newNode->node_data = data;
-		//printf("node data: %p\n", (int *) newNode->node_data);
 		queue->front = newNode;
 		queue->back = NULL;
 	}
 	else if (queue->queue_len == 1) {
-		//printf("queue->back is NULL\n");
 		struct node* newNode = (struct node*) malloc(sizeof (struct node));
 		newNode->prev = queue->front;
 		newNode->next = NULL;
 		newNode->node_data = data;
-		//printf("node data: %p\n", (int *) newNode->node_data);
 		queue->back = newNode;
 		queue->front->next = queue->back;
 	}
 	else {
-		//printf("QUEUE IS NOT EMPTY\n");
 		struct node * copy_back = queue->back;
 		struct node* newNode = (struct node*) malloc(sizeof (struct node));
 		newNode->prev = copy_back;
@@ -91,20 +83,16 @@ int queue_dequeue(queue_t queue, void **data)
 	if (queue->queue_len == 0 || queue == NULL || data == NULL || queue->front == NULL){
 		return -1;
 	}
-	*data = queue->front->node_data; //->node_data; <- can't do this
-	//printf("data: %p\n", queue->front->node_data);
+	*data = queue->front->node_data;
 	queue->front = queue->front->next;
 	queue->queue_len -= 1;
 	
 	return 0;
 }
 
-int queue_delete(queue_t queue, void *data) //need to free data whenever we "delete" it
+int queue_delete(queue_t queue, void *data) 
 {
-	// if (queue->queue_len == -1){
-	// 	return -2;
-	// }
-	// null cases
+	// need to free data whenever we "delete" it
 	if (queue->queue_len == 0 || queue == NULL || data == NULL){
 		return -1;
 	}
@@ -114,20 +102,18 @@ int queue_delete(queue_t queue, void *data) //need to free data whenever we "del
 	struct node* check;
 	check = queue->front;
 	while(check != NULL){
-		//printf("Hi 1! node data: %p data: %p\n", check->node_data, data);
 		if (check->node_data == data){
-			//printf("Hi!\n");
 			if (check == queue->front) {
 				queue->front = queue->front->next;
 				if (queue->front == queue->back)
 					queue->back = NULL;
 				else if (queue->front != NULL)
 					queue->front->prev = NULL;
-				free(check); //edit - not in csif yet
+				free(check);
 			}
 			else if (check->next == NULL) {
 				check->prev->next = NULL;
-				free(check); //edit - not in csif yet
+				free(check);
 			}
 			else {
 				struct node* temp_node;
@@ -136,12 +122,10 @@ int queue_delete(queue_t queue, void *data) //need to free data whenever we "del
 				check->prev->next = check->next;
 				// change check's next's prev to check's prev
 				check->next->prev = check->prev;
-				free(check); //edit - not in csif yet
+				free(check);
 				check = temp_node;
 			}
 			queue->queue_len -= 1;
-			//free(check);
-			//printf("Bye!\n");
 			return 0;
 		}
 		check = check->next;
@@ -159,25 +143,13 @@ int queue_iterate(queue_t queue, queue_func_t func)
 
 	struct node* check;
 	check = queue->front;
-	//printf("first val in queue: %d\n", *(int *) check->node_data);
-	// queue_t test;
-	// test->queue_len = -1;
 	while(check != NULL){
-		// int deletion_check = func(test, check);
-		// if (func(test, check) == -2){
-		// 	check = check->next;
-		// 	continue;
-		// }
 		if (check->node_data == NULL)
 			return -1;
-			//printf("seg fault\n");
 		void * initData = check->node_data;
-		//printf("Going to run func on data %d in queue\n", *(int *) check->node_data);
 		func(queue, check->node_data);
-		//printf("I ran func and got %d\n\n", *(int *) check->node_data);
 		if (check->node_data != initData)
 			return -1;
-			//printf("I deleted the data here\n");
 		check = check->next;
 	}
 
