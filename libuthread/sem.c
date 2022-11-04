1e696870404cbec812e604928a854bea901bcc38
HoaiAnHo <hoaianho3100@gmail.com>
2022-11-03 23:51:51 -0700

    Update sem.c
    make sem look nicer

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -8,16 +8,14 @@
 #include "uthread.h"
 
 struct semaphore {
-	/* TODO Phase 3 */
-	//queue_t sem_queue;
+	// queue_t sem_queue;
 	int lock;
 	size_t count;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 3 */
-	//queue_t sem_queue = queue_create();
+	// queue_t sem_queue = queue_create();
 	sem_t sem = (struct semaphore *) malloc(sizeof(struct semaphore));
 	sem->lock = 0;
 	sem->count = count;
@@ -26,9 +24,8 @@ sem_t sem_create(size_t count)
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 3 */
-
-	if (sem == NULL || sem->count != 0 || sem->lock) //if locks are still in use, return -1
+	// if locks are still in use, return -1
+	if (sem == NULL || sem->count != 0 || sem->lock) 
 		return -1;
 	
 	free(sem);
@@ -37,33 +34,31 @@ int sem_destroy(sem_t sem)
 
 int sem_down(sem_t sem)
 {
-	/* TODO Phase 3 */
 	if (sem == NULL)
 		return -1;
 
-	//spinlock_lock(sem->lock);
+	// spinlock_lock(sem->lock);
 	sem->lock = 1;
 	while (sem->count == 0) {
 		/* Block self */
 	}
 
 	sem->count -= 1;
-	//spinlock_unlock(sem->lock);
+	// spinlock_unlock(sem->lock);
 	return 0;
 }
 
 int sem_up(sem_t sem)
 {
-	/* TODO Phase 3 */
 	if (sem == NULL)
 		return -1;
 
-	//spinlock_lock(sem->lock);
+	// spinlock_lock(sem->lock);
 	sem->count += 1;
 
 	/* wake up first thread in line, if there are any*/
 
-	//spinlock_unlock(sem->lock);
+	// spinlock_unlock(sem->lock);
 	sem->lock = 0;
 	return 0;
 }
